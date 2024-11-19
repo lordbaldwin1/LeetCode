6cfcee1a4c803648344ad1c162c5a3e54ccdff1d
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2024-11-19 15:41:50 -0800

    Time: 0 ms (100%), Space: 52.7 MB (34.48%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,9 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // hold onto position where next element is placed, x
-    // compare if the hold == current number in iteration
-    // if they don't match, set hold to that number
     let x = 1;
     for(let i = 1; i < nums.length; i++) {
-        if(nums[i] !== nums[x-1]) {
+        if(nums[x - 1] !== nums[i]) {
             nums[x] = nums[i];
             x++;
         }
