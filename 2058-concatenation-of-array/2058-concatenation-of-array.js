0b76c31419d592bd8172f45a1083a1b5aafbfb3b
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2024-11-18 23:27:23 -0800

    Time: 2 ms (31.72%), Space: 53.3 MB (25.7%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -3,14 +3,6 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let i = nums.length;
-    let n = nums.length;
-    let t = 0;
-    let ans = nums;
-    for(let x = n; x < (i + n); x++) {
-        ans[x] = nums[t];
-        t++;
-    }
-    return nums;
-
+    let ans = [...nums, ...nums];
+    return ans;
 };
