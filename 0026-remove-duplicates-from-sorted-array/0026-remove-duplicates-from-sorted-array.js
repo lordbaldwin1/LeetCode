ece3f2b52598f8ebdf9140a92d95da9ecc065d4b
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2024-11-19 19:14:05 -0800

    Time: 1 ms (63.4%), Space: 51.7 MB (92.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let x = 1;
+    let t = 1;
+
     for(let i = 1; i < nums.length; i++) {
-        if(nums[x - 1] !== nums[i]) {
-            nums[x] = nums[i];
-            x++;
+        if(nums[i] !== nums[t - 1]) {
+            nums[t] = nums[i];
+            t++;
         }
     }
-    return x;
+    return t;
 };
