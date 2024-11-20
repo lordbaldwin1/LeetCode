30a049e5f0537185eb3a33db3480dccda0efcb89
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2024-11-19 19:40:26 -0800

    Time: 0 ms (100%), Space: 49.2 MB (24.16%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length > haystack.length) {
+        return -1;
+    }
+
+    for(let i = 0; i < haystack.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+    return -1;
+};
