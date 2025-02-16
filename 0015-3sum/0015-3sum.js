25549351198c7e2f511ae5e641cd056d930e1b21
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-02-15 16:35:50 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,12 +4,9 @@
  */
 var threeSum = function(nums) {
     let result = [];
-
     nums.sort((a, b) => a - b);
 
     for (let i = 0; i < nums.length; i++) {
-        // check if i is the same as previous number (catching duplicates)
-        // SKIP TO NEXT ITERATION IF TRUE
         if (i > 0 && nums[i] === nums[i - 1]) {
             continue;
         }
@@ -20,9 +17,8 @@ var threeSum = function(nums) {
             if (total === 0) {
                 result.push([nums[i], nums[l], nums[r]]);
                 l++;
-                // check for duplicates again
-                // if left is same as previous skip it
-                while (nums[l] === nums[l-1] && l < r) {
+
+                if (nums[l] === nums[l - 1] && l < r) {
                     l++;
                 }
             } else if (total < 0) {
