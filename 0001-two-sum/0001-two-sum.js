e31747968d4abdbaf997266deea34a496c8832e7
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-03-20 18:48:59 -0700

    Time: 0 ms (100%), Space: 54.7 MB (27.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   let map = {};
+    let map = {};
 
-        for (let i = 0; i < nums.length; i++) {
-            let diff = target - nums[i];
-
-            if (map[diff] !== undefined && map[diff] !== i) {
-                return [map[diff], i];
-            }
-
-            map[nums[i]] = i;
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (map[diff] !== undefined && map[diff] !== i) {
+            return [map[diff], i];
         }
-        return [];
+        map[nums[i]] = i;
+    }
+    return [];
 };
