8c5b9d977526483cd14c9494556c340dc5b492d1
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-06-18 20:42:14 -0700

    Time: 10 ms (82.18%), Space: 54.6 MB (98.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,11 +4,21 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split('').sort().join('');
-    t = t.split('').sort().join('');
-    if (s === t) {
+    if (s.length !== t.length) {
+            return false;
+        }
+
+        const countS = {};
+        const countT = {};
+        for (let i = 0; i < s.length; i++) {
+            countS[s[i]] = (countS[s[i]] || 0) + 1;
+            countT[t[i]] = (countT[t[i]] || 0) + 1;
+        }
+
+        for (const key in countS) {
+            if (countS[key] !== countT[key]) {
+                return false;
+            }
+        }
         return true;
-    } else {
-        return false;
-    }
 };
