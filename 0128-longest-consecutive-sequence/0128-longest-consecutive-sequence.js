0d22791307c24c2ae9080612bc9d1a25358fd868
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-06-19 22:12:52 -0700

    Time: 40 ms (57.64%), Space: 81 MB (50.31%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -5,15 +5,15 @@
 var longestConsecutive = function(nums) {
     let numSet = new Set(nums);
     let longest = 0;
-    let cur = 0;
-    
+
     for (const num of numSet) {
         if (!numSet.has(num - 1)) {
-            cur = 1;
-            while (numSet.has(num + cur)) {
-                cur += 1;
+            let streak = 1;
+            
+            while (numSet.has(num + streak)) {
+                streak++
             }
-        longest = Math.max(cur, longest);
+            longest = Math.max(longest, streak);
         }
     }
     return longest;
