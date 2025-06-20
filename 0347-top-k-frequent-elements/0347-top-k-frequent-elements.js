4fde248df2ffda955b69b160ae3d84d8c46a88fe
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-06-19 21:53:18 -0700

    Time: 12 ms (61.08%), Space: 60.2 MB (46.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,25 +4,18 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let count = {};
-    let freq = Array.from({ length: nums.length + 1 }, () => []);
+    const count = {};
 
-    for (let i = 0; i < nums.length; i++) {
-        count[nums[i]] = (count[nums[i]] || 0) + 1;
+    for (const n of nums) {
+        count[n] = (count[n] || 0) + 1;
     }
 
-    for (const n in count) {
-        freq[count[n]].push(parseInt(n));
-    }
+    const arr = Object.entries(count);
 
-    let result = [];
-    for (let i = freq.length - 1; i > 0; i--) {
-        for (const n of freq[i]) {
-            result.push(n);
-            if (result.length === k) {
-                return result;
-            }
-        }
-    }
-    return result;
+    const sortedArr = arr.sort((a, b) => b[1] - a[1]);
+
+    const slicedArr = arr.slice(0, k);
+
+    const res = slicedArr.map((p) => Number(p[0]));
+    return res;
 };
