c5f7875e85ce844db7e84be86d48d7fad4cea6c8
zachar7 <116612584+lordbaldwin1@users.noreply.github.com>
2025-07-16 15:11:51 -0700

    Time: 2 ms (46.48%), Space: 6 MB (19.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,15 +1,11 @@
 func twoSum(nums []int, target int) []int {
     numIdx := make(map[int]int)
-    for i := 0; i < len(nums); i++ {
-        diff := target - nums[i]
-        idx, ok := numIdx[diff]
-        if !ok {
-            numIdx[nums[i]] = i
-            continue
-        }
-        if idx != i {
+    for i, num := range nums {
+        diff := target - num
+        if idx, found := numIdx[diff]; found {
             return []int{idx, i}
         }
+        numIdx[num] = i
     }
-    return []int{}
+    return nil
 }
